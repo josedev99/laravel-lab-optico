a6a1d4a2257aa6da6ba683c9a49fcc828a08f7ef
jose dev <josededanes99@gmail.com>
2025-01-11 23:34:08 -0600

    Update iventario terminados

--- app/Http/Controllers/LenteTermController.php
+++ app/Http/Controllers/LenteTermController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\LenteTerm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class LenteTermController extends Controller
 {
@@ -41,8 +42,38 @@ class LenteTermController extends Controller
         }
     }
 
-    public function getLenteTerm(){
+    public function getLenteTerm() {
         $lentes_terminados = LenteTerm::get();
-        return response()->json($lentes_terminados);
+        $stock_term = [];
+    
+        foreach ($lentes_terminados as $item) {
+            $array = [
+                'id' => $item['id'],
+                'nombre' => $item['nombre'],
+                'marca' => $item['marca'],
+                'diseno' => $item['diseno'],
+                'esf_desde' => $item['esf_desde'],
+                'esf_hasta' => $item['esf_hasta'],
+                'cil_desde' => $item['cil_desde'],
+                'cil_hasta' => $item['cil_hasta'],
+            ];
+    
+            // Obtener los datos de stock
+            $stocks = DB::select("SELECT e.esfera, e.cilindro, SUM(e.stock) AS stock FROM existencias AS e WHERE e.lente_term_id = ? GROUP BY e.esfera, e.cilindro", [$item['id']]);
+    
+            // Transformar el resultado en un mapa anidado
+            $stockMap = [];
+            foreach ($stocks as $stock) {
+                $esfera = number_format($stock->esfera, 2);
+                $cilindro = number_format($stock->cilindro, 2);
+                $stockMap[$esfera][$cilindro] = (int) $stock->stock;
+            }
+    
+            $array['stocks'] = $stockMap;
+            $stock_term[] = $array;
+        }
+    
+        return response()->json($stock_term);
     }
+    
 }
